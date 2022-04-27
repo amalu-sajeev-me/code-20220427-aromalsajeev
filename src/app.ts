db4bc1bd69071f841f7430efda063fd41142bf80
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-04-27 14:56:06 +0530

    documentation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { middlewares } from "./middlewares";
+import { serveSwagger, setupSwagger } from "./middlewares/swagger";
 import { bmiRouter } from "./routers/bmi.router";
 
 const app = express();
 
 app.use(middlewares);
+app.use("/api-docs", serveSwagger, setupSwagger);
 app.use(bmiRouter);
 
 export { app };
--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,5 +1,71 @@
+const defaultData = [
+    {
+        "Gender": "Male",
+        "HeightCm": 171,
+        "WeightKg": 96
+    },
+    {
+        "Gender": "Male",
+        "HeightCm": 161,
+        "WeightKg": 85
+    },
+    {
+        "Gender": "Male",
+        "HeightCm": 180,
+        "WeightKg": 77
+    },
+    {
+        "Gender": "Female",
+        "HeightCm": 166,
+        "WeightKg": 62
+    },
+    {
+        "Gender": "Female",
+        "HeightCm": 150,
+        "WeightKg": 70
+        
+    },
+    {
+        "Gender": "Female",
+        "HeightCm": 167,
+        "WeightKg": 82
+    }
+];
+
 const swaggerDocument = {
-    // 
+    swagger: "2.0",
+    info: {
+        title: "BMI Calculator",
+        description: "calculate BMI",
+        version: "1.0.0"
+    },
+    paths: {
+        "/calc": {
+            post: {
+                summary: "get the BMI details calculated",
+                description: "",
+                parameters: [
+                    {
+                        in: "body",
+                        name: "userData",
+                        required: true,
+                        schema: {
+                            description: "user details",
+                            default: defaultData
+                        }
+                    }
+                ],
+                responses: {
+                    "200": {
+                        description: "",
+                        schema: {
+                            type: "object"
+                        }
+                    }
+                }
+            }
+        }
+    }
 };
 
 export { swaggerDocument };
